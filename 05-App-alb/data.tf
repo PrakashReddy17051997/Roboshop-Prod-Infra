e434a8132ac9b5e07ea7d25fefed7927a1eea40c
orakanti <orakanti@rptu.de>
2026-04-06 14:43:09 +0200

    added overwrite= true to replace the exisitng  ssm parameter

--- 05-App-alb/data.tf
+++ 05-App-alb/data.tf
@@ -6,6 +6,7 @@ data "aws_ssm_parameter" "private_subnet_ids" {
 
 data "aws_ssm_parameter" "app_alb_sg_id"{
   name = "/${var.project_name}/${var.environment}/app_alb_sg_id"
+  overwrite = true
 }
 data "aws_route53_zone" "selected" {
   name = var.zone_name
